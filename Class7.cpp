b51e401e2ea017c3ef0220257a7b3be41d276ff1
Maimoona <160620135+maimoona2024@users.noreply.github.com>
2024-04-09 07:11:31 +0500

    Update Class7.cpp

--- Class7.cpp
+++ Class7.cpp
@@ -1,28 +1,28 @@
 // Program # 1
 
 
-#include <iostream>
-using namespace std;
-int main() {
-   cout<<"Hello World!"<<endl;
-   cout<<"My name is Maimoona"<<endl;
-}
+// #include <iostream>
+// using namespace std;
+// int main() {
+//    cout<<"Hello World!"<<endl;
+//    cout<<"My name is Maimoona"<<endl;
+// }
 
 // Program # 2
 
 
-#include <iostream>
-using namespace std;
-int main(){
-int x;
-float y;
-x=5;
-y=10.0;
-cout<<x+y<<endl; 
-cout<<x-y<<endl;
-cout<<x*y<<endl;
-cout<<x/y<<endl;
-}
+// #include <iostream>
+// using namespace std;
+// int main(){
+// int x;
+// float y;
+// x=5;
+// y=10.0;
+// cout<<x+y<<endl; 
+// cout<<x-y<<endl;
+// cout<<x*y<<endl;
+// cout<<x/y<<endl;
+// }
 
 
 //Program # 3
@@ -43,37 +43,37 @@ cout<<x/y<<endl;
 //Program # 4
 
 
-#include <iostream>
-using namespace std;
-int main() {
-int x;
-x = 4;
-cout << x << endl;  //4
-x += 2;      
-cout << x << endl;  //6
-x -= 1;
-cout << x << endl;  // 5
-x *= 10;
-cout << x << endl; // 50
-}
+// #include <iostream>
+// using namespace std;
+// int main() {
+// int x;
+// x = 4;
+// cout << x << endl;  //4
+// x += 2;      
+// cout << x << endl;  //6
+// x -= 1;
+// cout << x << endl;  // 5
+// x *= 10;
+// cout << x << endl; // 50
+// }
 
 
 //Program # 5
 
-#include <iostream>
-using namespace std;
-int main() {
-    int x = 4;
-    cout<<x<<endl; //4
-    x++;
-    cout<<x<<endl; //5
-    x--;
-    cout<<x<<endl; //4
-    --x;
-    cout<<x<<endl; //3
-    ++x;
-    cout<<x<<endl; //4
-}
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int x = 4;
+//     cout<<x<<endl; //4
+//     x++;
+//     cout<<x<<endl; //5
+//     x--;
+//     cout<<x<<endl; //4
+//     --x;
+//     cout<<x<<endl; //3
+//     ++x;
+//     cout<<x<<endl; //4
+// }
 
 
 
