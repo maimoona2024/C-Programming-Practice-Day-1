6fcd382b02ab9624f595ff8b190a02b9eb658b0f
Maimoona <160620135+maimoona2024@users.noreply.github.com>
2024-04-09 07:09:23 +0500

    Add files via upload

---
+++ Class7.cpp
@@ -0,0 +1,80 @@
+// Program # 1
+
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//    cout<<"Hello World!"<<endl;
+//    cout<<"My name is Maimoona"<<endl;
+// }
+
+// Program # 2
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+// int x;
+// float y;
+// x=5;
+// y=10.0;
+// cout<<x+y<<endl; 
+// cout<<x-y<<endl;
+// cout<<x*y<<endl;
+// cout<<x/y<<endl;
+// }
+
+
+//Program # 3
+
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int x;
+//     x = 4;
+//     cout << x << endl;  //4
+//     x = x + 2;      
+//     cout << x << endl;  //6
+//     x = x + 1;
+//     cout << x << endl;  //7
+// }
+
+//Program # 4
+
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+// int x;
+// x = 4;
+// cout << x << endl;  //4
+// x += 2;      
+// cout << x << endl;  //6
+// x -= 1;
+// cout << x << endl;  // 5
+// x *= 10;
+// cout << x << endl; // 50
+// }
+
+
+// Program # 5
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int x = 4;
+//     cout<<x<<endl; //4
+//     x++;
+//     cout<<x<<endl; //5
+//     x--;
+//     cout<<x<<endl; //4
+//     --x;
+//     cout<<x<<endl; //3
+//     ++x;
+//     cout<<x<<endl; //4
+// }
+
+
+
+
